d3cc231f2b06343f631530af0fc7cf54f4616fc0
Jimmy Bonilla <jimmy.bonilla@bairesdev.com>
2023-01-13 18:25:31 -0600

    Comparision operators and conditional execution - completed

---
+++ Python-Essentials-1/Boolean-Values-Conditional-Execution-Loops-Lists-and-List-Processing-Logical-and-Bitwise-Operations/Section_1_Making_decisions_in_Python/Comparison_operators_and_conditional_execution.py
@@ -0,0 +1,19 @@
+plant_name = input()
+
+if plant_name == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif plant_name == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not {}!".format(plant_name))
+
+# Alternative solution
+# name = input("Enter flower name: ")
+
+# if name == "Spathiphyllum":
+#     print("Yes - Spathiphyllum is the best plant ever!")
+# elif name == "spathiphyllum":
+#     print("No, I want a big Spathiphyllum!")
+# else:
+#     print("Spathiphyllum! Not", name + "!")
+	
